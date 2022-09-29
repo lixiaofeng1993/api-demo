d8ab9644e2425aff92170480811ecfe3d28fcde4
lixiaofeng1993 <15235514553@163.com>
2022-09-29 22:44:28 +0800

    额1

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ async def startup_event():
 
 
 @app.on_event('startup')
-@repeat_task(seconds=60 * 1, wait_first=True)
+@repeat_task(seconds=60 * 30, wait_first=True)
 def repeat_task_aggregate_request_records() -> None:
     send_ding()
 
