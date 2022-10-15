f7e00643ea63f31ddcf7332b07abb50e81f8f227
lixiaofeng1993 <15235514553@163.com>
2022-10-15 15:24:44 +0800

    回复微信消息5

--- api/wechat.py
+++ api/wechat.py
@@ -44,7 +44,8 @@ async def handle_wx(signature, timestamp, nonce, echostr):
 
 @router.post("/", summary="回复微信消息")
 async def wx_msg(signature, timestamp, nonce, openid, request: Request):
-    logger.info(request.body())
+    logger.info(request.values())
+    logger.info(request.get("xml"))
     logger.info(f"signature: {signature} ==> {timestamp} == > {nonce} ==> {openid}")
     xml = {
         "xml": {
