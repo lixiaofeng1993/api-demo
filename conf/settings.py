75b3c1047c191072457898e68585d312749c80d0
lixiaofeng1993 <15235514553@163.com>
2022-10-16 16:24:03 +0800

    获取文章完成

--- conf/settings.py
+++ conf/settings.py
@@ -27,14 +27,13 @@ AppID = "wx99e27a845c7d8c52"
 AppSecret = "fff1120327c7297e536c44979a6273d3"
 
 FOLLOW = "这世界怎么那么多人，蹉跎回首，已不再年轻。\n这世界怎么那么多人，兜兜转转，浑噩半生。\n这世界怎么那么多人......\n\n" \
+         "回复 【<a href='weixin://bizmsgmenu?msgmenucontent=all&msgmenuid=all'>all</a>】，获取所有文章合集\n" \
          "回复 【<a href='weixin://bizmsgmenu?msgmenucontent=1993.05.16&msgmenuid=1993.05.16'>1993.05.16</a>】，" \
          "返回已经走过的天数信息\n" \
-         "回复 【<a href='weixin://bizmsgmenu?msgmenucontent=图片&msgmenuid=图片'>图片</a>】 or " \
-         "【<a href='weixin://bizmsgmenu?msgmenucontent=小七&msgmenuid=小七'>小七</a>】，返回小七美照\n" \
-         "回复 【<a href='weixin://bizmsgmenu?msgmenucontent=今天&msgmenuid=今天'>今天</a>】 or " \
-         "【<a href='weixin://bizmsgmenu?msgmenucontent=today&msgmenuid=today'>today</a>】，返回万年历当天信息\n" \
+         "回复 【<a href='weixin://bizmsgmenu?msgmenucontent=小七&msgmenuid=小七'>小七</a>】，返回小七美照\n" \
+         "回复 【<a href='weixin://bizmsgmenu?msgmenucontent=today&msgmenuid=today'>today</a>】，返回万年历当天信息\n" \
          "回复 【<a href='weixin://bizmsgmenu?msgmenucontent=放假&msgmenuid=放假'>放假</a>】，返回万年历当年放假安排\n" \
-         "回复 【<a href='weixin://bizmsgmenu?msgmenucontent=西部黄金&msgmenuid=西部黄金'>股票名称</a>】 or " \
+         "回复 【<a href='weixin://bizmsgmenu?msgmenucontent=西部黄金&msgmenuid=西部黄金'>股票名称</a>】 " \
          "【<a href='weixin://bizmsgmenu?msgmenucontent=601069&msgmenuid=601069'>股票代码</a>】，返回当天股票走势信息\n"
 ArticleUrl = "<a href='https://mp.weixin.qq.com/mp/appmsgalbum?__biz=Mzg3NDg2Njc3MQ==&action=getalbum&album_id=" \
              "2622547569440768001#wechat_redirect'>点击获取所有文章</a>"
