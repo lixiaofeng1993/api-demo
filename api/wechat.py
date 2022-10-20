e40bd16e6d0d39d6a53cfbe4df6d4e7ff3de04c9
lixiaofeng1993 <15235514553@163.com>
2022-10-20 14:00:49 +0800

    作者推荐1

--- api/wechat.py
+++ api/wechat.py
@@ -69,7 +69,7 @@ async def wx_msg(request: Request, signature, timestamp, nonce, openid, db: Sess
                 if len(content) >= 2000 and "</a>" in content:
                     content = content[len(content) - 2000:] + "..."
                 elif len(content) >= 300:
-                    content = content[:301]
+                    content = content[:500]
                 return Response(
                     Message(to_user, from_user, content=content).send(),
                     media_type="application/xml")
