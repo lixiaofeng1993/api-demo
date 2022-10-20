fa14930dc4254920d7eb0b2c3f0db6ac74259054
lixiaofeng1993 <15235514553@163.com>
2022-10-20 13:57:52 +0800

    回复字数限制2000

--- api/wechat.py
+++ api/wechat.py
@@ -66,10 +66,10 @@ async def wx_msg(request: Request, signature, timestamp, nonce, openid, db: Sess
             if not content:
                 content, media_id = send_wx_msg(db, request, rec_msg, token, skip)
             if rec_msg.MsgType == 'text' and not media_id:
-                if len(content) >= 2048 and "</a>" in content:
-                    content = content[len(content) - 2045:] + "..."
-                elif len(content) >= 2048:
-                    content = content[:2048]
+                if len(content) >= 2000 and "</a>" in content:
+                    content = content[len(content) - 2000:] + "..."
+                elif len(content) >= 2000:
+                    content = content[:2000]
                 return Response(
                     Message(to_user, from_user, content=content).send(),
                     media_type="application/xml")
