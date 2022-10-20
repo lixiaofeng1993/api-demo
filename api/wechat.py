7bb2d4a01e36ed4c9013a3a4261a75f8fe34ab1d
lixiaofeng1993 <15235514553@163.com>
2022-10-20 14:17:57 +0800

    更多完成啦

--- api/wechat.py
+++ api/wechat.py
@@ -66,10 +66,10 @@ async def wx_msg(request: Request, signature, timestamp, nonce, openid, db: Sess
             if not content:
                 content, media_id = send_wx_msg(db, request, rec_msg, token, skip)
             if rec_msg.MsgType == 'text' and not media_id:
-                # if "</a>" in content and len(content) >= 666:
-                #     content = content[len(content) - 666:] + "..."
-                # elif len(content) >= 666:
-                #     content = content[:666] + "..."
+                if "</a>" in content and len(content) >= 2000:
+                    content = content[len(content) - 2000:] + "..."
+                elif len(content) >= 666 and "</a>" not in content:
+                    content = content[:666] + "..."
                 return Response(
                     Message(to_user, from_user, content=content).send(),
                     media_type="application/xml")
