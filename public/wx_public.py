0ac9faf74e796f72c4ddc286b3f9844e5aa0f675
lixiaofeng1993 <15235514553@163.com>
2022-10-22 01:44:53 +0800

    推荐功能优化

--- public/wx_public.py
+++ public/wx_public.py
@@ -185,7 +185,7 @@ def poetry_by_author_id(db: Session, request: Request, author_id: str, skip: int
     content = ""
     if flag:
         author = crud_poetry.get_author_by_id(db, author_id)
-        content = author.name
+        content = author.dynasty.strip("\n") + author.name
     data_list = crud_poetry.get_poetry_by_author_id(db, author_id, skip=skip)
     if data_list:
         content += "\n诗词推荐：\n"
@@ -224,12 +224,12 @@ def send_author(db: Session, request: Request, data):
     """
     输入作者，返回作者简介及古诗词推荐
     """
-    content = data.name
+    content = ""
+    if data.dynasty:
+        content += data.dynasty.strip("\n") + data.name
     if data.introduce:
         introduce = data.introduce.split("►")[0] if "►" in data.introduce else data.introduce
         content += "\n介绍：\n" + introduce.strip("\n")
-    elif data.dynasty:
-        content += "\n朝代：" + data.dynasty.strip("\n")
     content += poetry_by_author_id(db, request, data.id, 0)
     return content
 
