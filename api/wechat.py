a395c82b86194ab2885c0482f3f15a8b7b1fa808
lixiaofeng1993 <15235514553@163.com>
2022-11-01 20:40:13 +0800

    回复微信字数限制

--- api/wechat.py
+++ api/wechat.py
@@ -78,7 +78,7 @@ async def wx_msg(request: Request, signature, timestamp, nonce, openid, db: Sess
                 if "</a>" in content and len(content) >= 2000:
                     content = "..." + content[len(content) - 2000:]
                 elif len(content) >= 666 and "</a>" not in content:
-                    content = content[:720] + "..."
+                    content = content[:710] + "..."
                 return Response(
                     Message(to_user, from_user, content=content).send(),
                     media_type="application/xml")
