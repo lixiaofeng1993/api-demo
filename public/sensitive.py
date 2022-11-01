797e8461126670515a036220bcd57a7b7229bfeb
lixiaofeng1993 <15235514553@163.com>
2022-11-01 20:26:35 +0800

    敏感词

--- public/sensitive.py
+++ public/sensitive.py
@@ -8,25 +8,31 @@
 import requests
 from public.log import logger
 
+sensitive_list = [
+    "贱比",
+]
+
 
 def sensitive_words(text: str = ""):
-    data = {
-        "content": text,
-    }
-
-    headers = {
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-    }
-    url = "http://www.zhipaiwu.com/index.php/Weijinci/postIndex.html"
-    res = requests.post(url, headers=headers, data=data, verify=False)
-    try:
-        response = res.json()
-        if response["code"] == 200:
-            msg_list = response["result"]["minganArr"]
-            if isinstance(msg_list, list):
-                logger.info(f"检测到的敏感词 ===>>> {msg_list}")
-                for msg in msg_list:
-                    text = text.replace(msg, "*" * len(msg))
-    except Exception as error:
-        logger.error(f"敏感词请求出现异常 ===>>> {error}")
+    # data = {
+    #     "content": text,
+    # }
+    #
+    # headers = {
+    #     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    # }
+    # url = "http://www.zhipaiwu.com/index.php/Weijinci/postIndex.html"
+    # res = requests.post(url, headers=headers, data=data, verify=False)
+    # try:
+    #     response = res.json()
+    #     if response["code"] == 200:
+    #         msg_list = response["result"]["minganArr"]
+    #         if isinstance(msg_list, list):
+    #             logger.info(f"检测到的敏感词 ===>>> {msg_list}")
+    #             for msg in msg_list:
+    #                 text = text.replace(msg, "*" * len(msg))
+    # except Exception as error:
+    #     logger.error(f"敏感词请求出现异常 ===>>> {error}")
+    for msg in sensitive_list:
+        text = text.replace(msg, "*" * len(text))
     return text
