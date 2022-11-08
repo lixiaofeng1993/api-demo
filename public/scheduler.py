bda1a7c8313547848c83ab0e44189663e45fb1e5
lixiaofeng1993 <15235514553@163.com>
2022-11-08 21:36:42 +0800

    定时任务

--- public/scheduler.py
+++ public/scheduler.py
@@ -37,5 +37,5 @@ interval_task = {
 }
 scheduler = AsyncIOScheduler(**interval_task)
 # 添加一个定时任务
-scheduler.add_job(stock, trigger='cron', hour="9-15", minute=32, args=[True], id="stock_job", replace_existing=True)
-scheduler.add_job(shares, trigger='interval', seconds=30, id="shares_job", replace_existing=True)
+scheduler.add_job(stock, trigger='cron', hour="9-21", minute="*", args=[True], id="stock_job", replace_existing=True)
+scheduler.add_job(shares, trigger='interval', seconds=60 * 5, id="shares_job", replace_existing=True)
